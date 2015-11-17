197f1c2df4a2dd6d79c9a9d232f96e0a30e85657
Lou Alicegary <lou@ginkgotree.com>
2015-11-17 14:19:24 -0500

    Added Mirage support and fixed structure of return data from tutorial (was using "data" master key in JSON response for some reason)

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -1,19 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({  
-    
     model() {
-        let todos = [
-            {
-                title: "Learn Ember",
-                complete: false,
-            },
-            {
-                title: "Solve World Hunger",
-                complete: false,
-            }
-        ];
-        return todos;
+        return this.store.findAll('todo');
     }
-
 });
