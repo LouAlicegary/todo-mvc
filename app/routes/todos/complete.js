9fbe22f34b8bba8bb814e1b3d22207a263741bce
Lou Alicegary <lou@ginkgotree.com>
2015-11-17 16:18:15 -0500

    adding filters for complete and incomplete tasks

--- app/routes/todos/complete.js
+++ app/routes/todos/complete.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend({  
+    
+    model() {
+        return this.store.filter('todo', function(todo) {
+            return todo.get('complete');
+        });
+    },
+    
+    renderTemplate(controller, model) {
+        this.render('todos.index', {
+            model: model
+        });
+    }
+
 });
--- app/routes/todos/incomplete.js
+++ app/routes/todos/incomplete.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend({  
+    
+    model() {
+        return this.store.filter('todo', function(todo) {
+            return !todo.get('complete');
+        });
+    },
+    
+    // Renders the todos/index.hbs template in lieu of totos/incomplete.hbs
+    renderTemplate(controller, model) {
+        this.render('todos.index', {
+            model: model
+        });
+    }
+
 });
